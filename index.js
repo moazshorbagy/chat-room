f2b289056b5d3d4d3e986629afa31a692e75a79a
OmarFarrag <omar.alaa.farrag@gmail.com>
2019-04-28 17:02:16 +0200

    .

--- index.js
+++ index.js
@@ -10,16 +10,40 @@ var server = app.listen(4000, function(){
 // Static files
 app.use(express.static('public'));
 
+
+
 // Socket setup & pass server
 var io = socket(server);
+
+//middleware
+io.use((socket, next) => {
+    let token = socket.handshake.query.token;
+    if (token == "hi") {
+      return next();
+    }
+    return next(new Error('authentication error'));
+  });
+
+
 io.on('connection', (socket) => {
 
     console.log('made socket connection', socket.id);
+    
+    // send the list of online users to the new user
+    io.sockets.connected[socket.id].emit("onlineUsers", onlineUsers);
+
+    // Add the new user to the list of online users
+    const username = getUsernameByToken(socket.handshake.query.token);
+    onlineUsers.push(username);
+
+    socket.broadcast.emit('userOnline', username);
 
     // Handle chat event
     socket.on('chat', function(data){
         // console.log(data);
+        console.log(socket.handshake.query.token);
         io.sockets.emit('chat', data);
+
     });
 
     // Handle typing event
@@ -27,4 +51,19 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('typing', data);
     });
 
+    socket.on('disconnect', function(){
+        console.log("hi");
+        const disconnectedUsername = getUsernameByToken(socket.handshake.query.token);
+        socket.broadcast.emit('userOffline', disconnectedUsername);
+        onlineUsers.splice(onlineUsers.indexOf(disconnectedUsername),1);
+       
+    });
+
 });
+
+const getUsernameByToken = function(token){
+    return "user"+Math.random();
+}
+var onlineUsers = [];
+
+
--- public/chat.js
+++ public/chat.js
@@ -1,12 +1,13 @@
 // Make connection
-var socket = io.connect('https://daaffec8.ngrok.io/');
+var socket = io.connect('http://localhost:4000?token='+getCookie("token"));
 
 // Query DOM
 var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
       output = document.getElementById('output'),
-      feedback = document.getElementById('feedback');
+      feedback = document.getElementById('feedback')
+      onlineUsers = document.getElementById('online-users');
 
 // Emit events
 btn.addEventListener('click', function(){
@@ -36,3 +37,62 @@ socket.on('chat', function(data){
 socket.on('typing', function(data){
     feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
+
+
+socket.on('userOnline', function(data){
+    onlineUsers.innerHTML += '<p class="w3-bar-item w3-button">'+ data + '</p>';
+});
+
+socket.on('userOffline', function(data){
+    var parent = document.getElementById('online-users');
+    var child = getElementByInnerText(data);
+    parent.removeChild(child);
+});
+
+
+/**
+    * 
+    *
+    * @param  JS
+*/
+socket.on('onlineUsers', function(onlineUsersArr){
+    console.log("hi");
+    onlineUsersArr.forEach(element => {
+        onlineUsers.innerHTML += '<p class="w3-bar-item w3-button">'+ element + '</p>';
+   });
+});
+
+
+
+/**
+    * Gets the value of a cookie specifiec by name
+    *
+    * @param  {cname} name of the cookie
+*/
+function getCookie (cname) {
+    return "hi";
+    var name = "";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+
+/**
+    * Returns the DOM element specified by the inner text
+    *
+    * @param  {text} inner text of the element
+*/
+function getElementByInnerText(text){
+    var xpath = "//a[text()='"+text+"']";
+    return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  }
